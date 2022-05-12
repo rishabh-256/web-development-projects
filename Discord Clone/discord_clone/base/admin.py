ee997d95926ecbdfbd44548f38c17a3c5317b6d2
unknown <0910rishabh@gmail.com>
2022-05-13 03:41:10 +0530

    Initalizing Discord Clone Code

---
+++ Discord Clone/discord_clone/base/admin.py
@@ -0,0 +1,10 @@
+from pydoc_data.topics import topics
+from django.contrib import admin
+
+
+# Register your models here.
+from .models import Room, Topic, Message
+
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
